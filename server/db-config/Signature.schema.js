564e02d66d00d8a69b3ae49bb9959c10e3ecc1d4
AbhijathBenhur <abhijath.benhur@tellius.com>
2021-05-29 13:03:21 +0530

    move server out

---
+++ server/db-config/Signature.schema.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Signature = new Schema(
+    {
+        owner: { type: String, required: true },
+        title: { type: String, required: true },
+        category: { type: String , required: true},
+        description: { type: String, required: true },
+        price: { type: Number , required: true},
+        thumbnail: { type: String , required: true},
+        PDFFile: { type: String ,required: true},
+        PDFHash: { type: String ,required: true},
+        transactionID: { type: String},
+        ideaID: { type: String}
+    },
+    { timestamps: true },
+)
+
+module.exports = mongoose.model('Signature', Signature)
