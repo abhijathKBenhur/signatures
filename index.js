d35b7b3f22f51022dc8e584a4d5f11e564d07dde
AbhijathBenhur <abhijath.benhur@tellius.com>
2021-05-30 02:33:31 +0530

    ready for deployemnt

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongotConnection = require('./db-config/mongodb')
 const tokenAPI = require('./routes/TokenAPIs')
 const userAPI = require('./routes/UserAPI')
 const dotenv = require('dotenv')
-
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
@@ -15,18 +15,17 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
-app.get('/',(req,res) =>{
-  res.send("The backend server is up and running at :: " + process.env.PORT)
-})
+
 
 app.use('/api', tokenAPI)
 app.use('/api', userAPI)
 
 if(process.env.NODE_ENV == "production"){
-  app.use(express.static('build'))
+  app.use(express.static('client/build'))
+  app.get('*',(req,res) =>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
 }
-
-
 
 app.listen(PORT, function() {
   console.log("Server is running on Port: " + PORT);
