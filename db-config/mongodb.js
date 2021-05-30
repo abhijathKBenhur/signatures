67659426856ebc98389bdf06fd6bec87bf6a991c
AbhijathBenhur <abhijath.benhur@tellius.com>
2021-05-30 10:50:03 +0530

    changes in package config for heroku

--- db-config/mongodb.js
+++ db-config/mongodb.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 const MONGO_DB = "Signatures";
 
-console.log("Initializing mongo connection ");
+console.log("Initializing mongo connection with " + process.env.MONGODB_URL);
 const baseUrl = "mongodb://127.0.0.1:27017/Signatures";
+
 mongoose.connect(process.env.MONGODB_URL || baseUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
--- index.js
+++ index.js
@@ -18,7 +18,9 @@ app.use(cors());
 app.use("/api", tokenAPI);
 app.use("/api", userAPI);
 
+console.log("Checking node environment ::" + process.env.NODE_ENV)
 if (process.env.NODE_ENV == "production") {
+  console.log("Found node environment as" + process.env.NODE_ENV)
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
