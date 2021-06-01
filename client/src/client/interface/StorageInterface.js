dd1b37c42916d396be28e142a537c5a542009af3
AbhijathBenhur <abhijath.benhur@tellius.com>
2021-06-01 18:39:13 +0530

    add multer, cloudinary and ipfs integration

---
+++ client/src/client/interface/StorageInterface.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import _ from "lodash";
+import IPFS from "../config/ipfs";
+const api = axios.create({
+  baseURL: "http://localhost:4000/api",
+});
+
+const fileAPI = axios.create({
+  baseURL: "http://localhost:4000/api",
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
+});
+
+const getFilePathsFromIPFS = (form) => {
+  return new Promise((resolve, reject) => {
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(form.PDFFile);
+    reader.onloadend = () => {
+      IPFS.files.add(Buffer(reader.result), (error, result) => {
+        if (error) {
+          console.error(error);
+          reject(error);
+        }
+        console.log("PDF uploaded to IPFS")
+        resolve({ path: result[0].path, type: "PDFFile" });
+      });
+    };
+  });
+};
+
+export const getFilePaths = (form) => {
+  let promiseList = [];
+  promiseList.push(getPDFFilepath(form));
+  promiseList.push(getImagePath(form));
+
+  return Promise.all(promiseList);
+};
+
+export const getImagePath = (form) => {
+  const IMGformData = new FormData();
+  IMGformData.append("thumbnail", form.thumbnail);
+  IMGformData.append("hash", form.PDFHash);
+  IMGformData.append("type", "thumbnail");
+  return fileAPI.post(`/getCloundinaryPath`, IMGformData);
+}
+
+export const getPDFFilepath =(form) =>  {
+  let PDFformData = new FormData();
+  PDFformData.append("PDFFile", form.PDFFile);
+  PDFformData.append("hash", form.PDFHash);
+  PDFformData.append("type", "PDFFile");
+  return getFilePathsFromIPFS(form);
+}
+
+const StorageInterface = {
+  getFilePaths,
+  getImagePath,
+  getPDFFilepath
+};
+
+export default StorageInterface;
