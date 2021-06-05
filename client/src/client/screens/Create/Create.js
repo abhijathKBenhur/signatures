f48b00c9e3cb23ae32a1cec66d36f9165ab626d2
AbhijathBenhur <abhijath.benhur@tellius.com>
2021-06-05 11:09:25 +0530

    build update

--- client/src/client/screens/Create/Create.js
+++ client/src/client/screens/Create/Create.js
@@ -15,89 +15,94 @@ import _ from "lodash";
 import { toast } from "react-toastify";
 import Select from "react-select";
 
-class Create extends Component {
-  constructor(props) {
-    super(props);
+
+function Create(props) {
+  const [form, setFormData] = useState({
+    owner: "",
+    title: "",
+    category: [],
+    description: "",
+    price: 0,
+    thumbnail: undefined,
+    PDFFile: undefined,
+    PDFHash: undefined,
+    ideaID: undefined,
+    transactionID: undefined,
+  });
+  const [slideCount, setSlideCount] = useState(0);
+  let history = useHistory();
+
+  useEffect(() => {
     pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
     BlockChainInterface.getAccountDetails()
       .then((metamaskID) => {
-        this.setState({
+        setFormData({
+          ...form,
           owner: metamaskID,
         });
       })
       .catch((error) => {
         console.log(error);
       });
-    this.state = {
-      owner: "",
-      title: "",
-      category: [],
-      description: "",
-      price: 0,
-      thumbnail: undefined,
-      PDFFile: undefined,
-      PDFHash: undefined,
-      ideaID: undefined,
-      transactionID: undefined,
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleTagsChange = this.handleTagsChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.PDFLoadError = this.PDFLoadError.bind(this);
-    this.onDocumentLoadSuccess = this.onDocumentLoadSuccess.bind(this);
 
-    this.onImageDrop = (acceptedFiles) => {
-      this.setState({
-        thumbnail: Object.assign(acceptedFiles[0], {
-          preview: URL.createObjectURL(acceptedFiles[0]),
-        }),
-      });
-    };
+  }, []);
 
-    this.onPDFDrop = (acceptedFiles) => {
-      this.setState({
-        PDFFile: acceptedFiles[0],
-      });
-      const reader = new window.FileReader();
-      reader.readAsArrayBuffer(acceptedFiles[0]);
-      reader.onloadend = () => {
-        Hash.of(Buffer(reader.result)).then((PDFHashValue) => {
-          // Check for already existing PDF Hashes
-          this.setState({
-            PDFHash: PDFHashValue,
-          });
+
+  onImageDrop = (acceptedFiles) => {
+    setFormData({
+      ...form,
+      thumbnail: Object.assign(acceptedFiles[0], {
+        preview: URL.createObjectURL(acceptedFiles[0]),
+      }),
+    });
+  };
+
+  onPDFDrop = (acceptedFiles) => {
+    setFormData({
+      ...form,
+      PDFFile: acceptedFiles[0],
+    });
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(acceptedFiles[0]);
+    reader.onloadend = () => {
+      Hash.of(Buffer(reader.result)).then((PDFHashValue) => {
+        // Check for already existing PDF Hashes
+        setFormData({
+          ...form,
+          PDFHash: PDFHashValue,
         });
-      };
+      });
     };
+  };
+
+  PDFLoadError = (error) => {
+
+  }
+  onDocumentLoadSuccess = (success) => {
+
   }
 
-  PDFLoadError(error) {}
-  onDocumentLoadSuccess(success) {}
-
-  handleTagsChange(tags) {
-    this.setState({
+  handleTagsChange = (tags) => {
+    setFormData({
       category: JSON.stringify(tags),
     });
   }
 
-  handleChange(event) {
+  handleChange = (event)  => {
     var stateObject = function() {
       let returnObj = {};
       returnObj[this.target.name] = this.target.value;
       return returnObj;
     }.bind(event)();
-    this.setState(stateObject);
+    this.setFormData({...form,stateObject});
   }
 
-  closePopup() {}
-
-  handleSubmit(event) {
+  handleSubmit = (event) => {
     event.preventDefault();
-    let stateCopy = _.cloneDeep(this.state);
-    this.onSubmit(stateCopy);
+    onSubmit(form);
   }
 
-  updateIdeaIDToMongo(payload) {
+  updateIdeaIDToMongo = (payload) => {
     MongoDBInterface.updateIdeaID(payload)
       .then((success) => {
         toast.dark("Your thoughts are live on blockchain.", {
@@ -109,14 +114,14 @@ class Create extends Component {
           draggable: true,
           progress: undefined,
         });
-        this.props.history.push("/home");
+        history.push("/home");
       })
       .catch((err) => {
         console.log(err);
       });
   }
 
-  saveToMongo(form) {
+  saveToMongo = (form) => {
     MongoDBInterface.addSignature(form)
       .then((success) => {
         toast.dark("Your thoughts have been submitted!", {
@@ -128,22 +133,22 @@ class Create extends Component {
           draggable: true,
           progress: undefined,
         });
-        this.props.history.push("/home");
+        history.push("/home");
       })
       .catch((err) => {
         console.log(err);
       });
   }
 
-  saveToBlockChain(form) {
+  saveToBlockChain =(form) => {
     BlockChainInterface.publishIdea(
       form,
-      this.saveToMongo,
-      this.updateIdeaIDToMongo
+      saveToMongo,
+      updateIdeaIDToMongo
     );
   }
 
-  async onSubmit(form) {
+  onSubmit = (form) => {
     console.log("form:", form);
     form.IPFS = true;
     const parentThis = this;
@@ -165,189 +170,27 @@ class Create extends Component {
       });
   }
 
-  render() {
     return (
       <Container>
-        <Form
-          noValidate
-          encType="multipart/form-data"
-          onSubmit={this.handleSubmit}
-          className="createModal"
-        >
-          <Col md="12" className="form-col">
-            <Row>
-              <Col md="6">
-                <Row className="form-row">
-                  <h1>Post us your idea</h1>
-                </Row>
-                <Row className="form-row">
-                  <Form.Group
-                    as={Col}
-                    className="formEntry"
-                    md="12"
-                    controlId="title"
-                  >
-                    <Form.Control
-                      type="text"
-                      name="title"
-                      placeholder="Title"
-                      onChange={this.handleChange}
-                    />
-                  </Form.Group>
-                </Row>
-                <Row className="form-row">
-                  <Form.Group
-                    as={Col}
-                    className="formEntry"
-                    md="12"
-                    controlId="description"
-                  >
-                    <InputGroup>
-                      <Form.Control
-                        className="descriptionArea"
-                        as="textarea"
-                        rows={5}
-                        aria-describedby="inputGroupAppend"
-                        name="description"
-                        placeholder="Description"
-                        onChange={this.handleChange}
-                      />
-                    </InputGroup>
-                  </Form.Group>
-                </Row>
-
-                {/* <Row className="form-row additional-row">
-              <Col md="4">
-                <hr></hr>
-              </Col>
-              <Col md="4" className="additionaText">Additional information</Col>
-              <Col md="4">
-                <hr></hr>
-              </Col>
-            </Row> */}
-                <Row className="form-row">
-                  <Col md="7">
-                    <Form.Row className="imageContainer">
-                      <Dropzone
-                        onDrop={this.onImageDrop}
-                        acceptedFiles={".jpeg"}
-                        className="dropzoneContainer"
-                      >
-                        {({ getRootProps, getInputProps }) => (
-                          <section className="container">
-                            {!this.state.thumbnail && (
-                              <div
-                                {...getRootProps()}
-                                className="dropZone h-100"
-                              >
-                                <input {...getInputProps()} />
-                                <p>Thumbnail</p>
-                              </div>
-                            )}
-                          </section>
-                        )}
-                      </Dropzone>
-                      {this.state.thumbnail && (
-                        <img
-                          src={this.state.thumbnail.preview}
-                          alt="preview"
-                          className="droppedImage"
-                          style={{ width: "90%" }}
-                        />
-                      )}
-                    </Form.Row>
-                  </Col>
-                  <Col md="5">
-                    <Row className="form-row">
-                      <Form.Group as={Col} className="formEntry" md="12">
-                        <Select
-                          closeMenuOnSelect={true}
-                          isMulti
-                          options={CONSTANTS.CATEGORIES}
-                          onChange={this.handleTagsChange}
-                          placeholder="Tags"
-                        />
-                      </Form.Group>
-                    </Row>
-                    <Row className="form-row">
-                      <Form.Group as={Col} className="formEntry" md="12">
-                        <InputGroup className="">
-                          <Form.Control
-                            type="number"
-                            placeholder="Price"
-                            min={1}
-                            aria-label="Amount (ether)"
-                            name="price"
-                            onChange={this.handleChange}
-                          />
-                        </InputGroup>
-                      </Form.Group>
-                    </Row>
-                  </Col>
-                </Row>
-              </Col>
-              <Col md="1" className="separator"></Col>
-              <Col
-                md="5"
-                className="right-container d-flex justify-content-end"
-              >
-                {!this.state.PDFFile && (
-                  <Form.Row className="pdfContainer">
-                    <Dropzone
-                      onDrop={this.onPDFDrop}
-                      acceptedFiles={".pdf"}
-                      className="dropzoneContainer"
-                    >
-                      {({ getRootProps, getInputProps }) => (
-                        <section className="container h-100 ">
-                          <div
-                            {...getRootProps()}
-                            className="emptypdf dropZone h-100"
-                          >
-                            <input {...getInputProps()} />
-                            <p className="m-0">Drop files here</p>
-                          </div>
-                        </section>
-                      )}
-                    </Dropzone>
-                  </Form.Row>
-                )}
-                {this.state.PDFFile && (
-                  <Form.Row className="w-100 p15 d-flex justify-content-end">
-                    {this.state.PDFFile && (
-                      <div className="pdfUploaded h-100">
-                        <Document
-                          file={this.state.PDFFile}
-                          onLoadError={this.PDFLoadError}
-                          onLoadSuccess={this.onDocumentLoadSuccess}
-                        >
-                          <Page pageNumber={1} width={window.innerWidth / 4} />
-                        </Document>
-                      </div>
-                    )}
-                  </Form.Row>
-                )}
-              </Col>
-            </Row>
-            <Row>
-              <Col md="4"></Col>
-              <Col md="4 footerComponent">
-                <Button
-                  variant="danger"
-                  className="publish-btn"
-                  value="Submit"
-                  onClick={this.handleSubmit}
-                >
-                  Publish
-                </Button>
-              </Col>
-              <Col md="4"></Col>
-            </Row>
+        <div className="createform  d-flex align-items-center justify-content-center">
+          <Col md="10" sm="12" lg="10" xs="12 responsive-content">
+            <Form
+              noValidate
+              encType="multipart/form-data"
+              onSubmit={handleSubmit}
+              className="create-form"
+            >
+              <Row>
+                <Col md="12">wizart guide</Col>
+                <Col md="6">b</Col>
+                <Col md="6">a</Col>
+                <Col md="12">button section</Col>
+              </Row>
+            </Form>
           </Col>
-        </Form>
+        </div>
       </Container>
     );
   }
-}
 
 export default Create;
