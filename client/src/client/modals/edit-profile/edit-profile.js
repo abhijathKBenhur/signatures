0e49e29a6e4ece20c8fea969bac27e7a0a91a5e7
garuav <gbgaurav461@gmail.com>
2021-07-21 15:17:43 +0530

    changes for clan

---
+++ client/src/client/modals/edit-profile/edit-profile.js
@@ -0,0 +1,202 @@
+import React, { useEffect, useState } from 'react';
+import {
+    Modal,
+    Form,
+    OverlayTrigger,
+    Tooltip,
+    Button,
+    Col,
+    Row,
+    InputGroup,
+    Dropdown
+  } from "react-bootstrap";
+
+  import './edit-profile.scss';
+const EditProfile = ({ ...props }) => {
+
+    const [editProfile, setEditProfile] = useState({
+        profilePic: null,
+        userName: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+        facebookUrl: '',
+        linkedInUrl: "",
+        bio: ''
+    });
+
+    useEffect(() => {
+        const profileData = {
+            profilePic: props.profilePic || '',
+            userName: props.userName || '',
+            firstName: props.firstName ||  '',
+            lastName: props.lastName || '',
+            email: props.email || '',
+            facebookUrl: props.facebookUrl ||'',
+            linkedInUrl: props.linkedInUrl ||"",
+            bio: props.bio ||''
+        }
+
+        setEditProfile({...editProfile, profileData})
+    },[])
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setEditProfile({...editProfile, [name]: value});
+    }
+
+    const editProfileHandler = () => {
+        console.log('editProfile = ', editProfile)
+    }
+    
+    return (
+        <Modal
+          show={true}
+          onHide={props.onHide}
+          size="md"
+          className="edit-profile-modal"
+          dialogClassName="edit-profile-modal-dialog"
+          centered
+        >
+          <Modal.Body className="edit-profile-modal-body">
+            <div className="modal-header-wrapper">
+            <h4>Edit Profile </h4>
+              <div className="image-placeholder">
+
+              </div>
+            </div>
+            <div className="wrapper">
+            <Row className="username-row mb-4">
+                <Col md="12" className="">
+                  <div className="username-label second-grey">
+                    <Form.Label>
+                     UserName
+                    </Form.Label>
+                  </div>
+                  <Form.Control
+                    type="text"
+                    name="userName"
+                    value={editProfile.userName}
+                    
+                    disabled
+                  />
+                  </Col>
+                </Row>
+
+                 <Row className="name-row mb-4">
+                <Col md="6" xs= "12" className="">
+                  <div className="name-label second-grey">
+                    <Form.Label>
+                     First Name
+                    </Form.Label>
+                  </div>
+                  <Form.Control
+                    type="text"
+                    name="firstName"
+                    value={editProfile.firstName}
+                    onChange={(e) => handleChange(e)}
+                    
+                  />
+                  </Col>
+                  <Col md="6" xs= "12" className="">
+                  <div className="name-label second-grey">
+                    <Form.Label>
+                     Last Name
+                    </Form.Label>
+                  </div>
+                  <Form.Control
+                    type="text"
+                    name="lastName"
+                    value={editProfile.lastName}
+                    onChange={(e) => handleChange(e)}
+                    
+                  />
+                  </Col>
+                </Row>
+                
+                <Row className="email-row mb-4">
+                <Col md="12" className="">
+                  <div className="email-label second-grey">
+                    <Form.Label>
+                     Email
+                    </Form.Label>
+                  </div>
+                  <Form.Control
+                    type="text"
+                    name="email"
+                    value={editProfile.email}
+                    onChange={(e) => handleChange(e)}
+                  />
+                  </Col>
+                </Row>
+
+                <Row className="facebook-url-row mb-4">
+                <Col md="12" className="">
+                  <div className="facebook-label second-grey">
+                    <Form.Label>
+                     Facebook URL
+                    </Form.Label>
+                  </div>
+                  <Form.Control
+                    type="text"
+                    name="facebookUrl"
+                    value={editProfile.facebookUrl}
+                    onChange={(e) => handleChange(e)}
+                  />
+                  </Col>
+                </Row>
+
+                <Row className="linkedin-url-row mb-4">
+                <Col md="12" className="">
+                  <div className="linkedin-label second-grey">
+                    <Form.Label>
+                     LinkedIn URL
+                    </Form.Label>
+                  </div>
+                  <Form.Control
+                    type="text"
+                    name="linkedInUrl"
+                    value={editProfile.linkedInUrl}
+                    onChange={(e) => handleChange(e)}
+                  />
+                  </Col>
+                </Row>
+
+
+                <Row className="bio-row mb-4">
+                <Col md="12" className="">
+                  <div className="bio-label second-grey">
+                    <Form.Label>
+                     Bio
+                    </Form.Label>
+                  </div>
+                  <Form.Control
+                    as="textarea"
+                    rows={7}
+                    name="bio"
+                    style={{ resize: "none" }}
+                    value={editProfile.bio}
+                    onChange={(e) => handleChange(e)}
+                  />
+                  </Col>
+                </Row>
+
+                <Row className="button-section  d-flex mb-4">
+              <Col xs="12" className="button-bar">
+                <Button className="cancel-btn mr-2" onClick={props.onHide}>Cancel</Button>
+                <Button
+                  className="submit-btn"
+                  onClick={() => editProfileHandler()}
+                >
+                  Submit
+                </Button>
+              </Col>
+            </Row>
+            </div>
+          </Modal.Body>
+        </Modal>
+      );
+}
+
+export default EditProfile;
