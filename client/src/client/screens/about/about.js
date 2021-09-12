f2213f5988253de812edc3c0cdb55abd9340630b
abhijathbenhur <abhijath.benhur@tellius.com>
2021-09-12 23:54:09 +0530

    corrections

--- client/src/client/screens/about/about.js
+++ client/src/client/screens/about/about.js
@@ -17,53 +17,58 @@ const About = () => {
     RelationsInterface.subscribe({
       mailID: searchText,
     }).then((success) => {
-      showToaster("Sucbscribed!", { type: "dark" });
       setSubscribed(true);
-      setSubscribed(subscribedCount + 1)
+      setSubscribed(subscribedCount + 1);
     });
   };
 
   useEffect(() => {
-    RelationsInterface.getPrelaunches().then((success) => {
-      setSubscribedCount(_.get(success,'data.data').length)
-    }).catch(err => {
-      setSubscribedCount(0);
-
-    })
+    RelationsInterface.getPrelaunches()
+      .then((success) => {
+        setSubscribedCount(_.get(success, "data.data").length);
+      })
+      .catch((err) => {
+        setSubscribedCount(0);
+      });
   }, []);
 
   const type = (event) => {
     if (event) setSearchText(event.target.value);
   };
   return (
-    <Container className="about-container">
+    <Container fluid className="about-container">
       <Row className="about-item about-second">
-        <Col md="6" lg="6" sm="6">
+        <Col md="7" lg="8" sm="12">
           <div className="left">
             <span className="father-grey">IdeaTribe</span>
             {/* <span className="master-header">About Us</span> */}
             <span className="second-header mb-5 color-secondary-light">
               All your best ideas. On the blockchain. Forever.{" "}
             </span>
-            <span className="readable-text mb-3">
-            Great ideas can come to anyone, anywhere. And inspiration is fleeting. 
-            <br/>
-            <br/>
-            So, IdeaTribe makes it easy to register your idea on the blockchain. Instantly. 
-            <br/>
-            <br/>
-            This will protect you from copycats.
-            <br/>
-            <br/>
-            Not only can you stake claim on your ideas, you can also collaborate with others! After all, magic is when great minds come together.
-            <br/>
-            <br/>
+            <span className="readable-text mb-1">
+              Great ideas can come to anyone, anywhere. And inspiration is
+              fleeting.
+              <br />
+              <br />
+              So, IdeaTribe makes it easy to register your idea on the
+              blockchain. Instantly.
+              <br />
+              <br />
+              This will protect you from copycats.
+              <br />
+              <br />
+              Not only can you stake claim on your ideas, you can also
+              collaborate with others! After all, magic is when great minds come
+              together.
+              <br />
+              The founders of IdeaTribe are artists, musicians, writers,
+              businesspeople, product specialists and engineering talent who
+              have come together for one common purpose, to help you to bring
+              your ideas to reality.
+              <br />
+              <br />
             </span>
-            The founders of IdeaTribe are artists, musicians, writers, businesspeople, product specialists and engineering talent who have come together for one common purpose,
-            to help you to bring your ideas to reality.
-            <br/>
-            <br/>
-            Share your email and we will let you know when we launch. 
+
             <br></br>
             {/* <br></br><br></br>
         
@@ -75,13 +80,15 @@ const About = () => {
             <Col>
               {subscribed ? (
                 <Row>
-                <div className="subscribe-block master-grey color-secondary subscribed">Thank you for sharing your email. We will be in touch! !</div>
+                  <div className="subscribe-block master-grey color-secondary subscribed">
+                    Thank you for sharing your email. We will be in touch!
+                  </div>
                 </Row>
               ) : (
                 <div className="">
                   {/* <Row className="subscribe-block master-header justify-content-center">Subscribe</Row> */}
                   <Row className="subscribe-block master-grey ">
-                    Type in your email and we will keep you posted.
+                    Type your email and we will keep you posted.
                   </Row>
                   <Row className="mt-3 subscribe-container">
                     <Col className="p-0">
@@ -92,7 +99,7 @@ const About = () => {
                         autoComplete="off"
                         className="search-box w-100 h-100"
                         placeholder="Email"
-                        style={{paddingLeft:"10px"}}
+                        style={{ paddingLeft: "10px" }}
                       />
                     </Col>
                     <Col className="p-0">
@@ -109,14 +116,14 @@ const About = () => {
                     </Col>
                   </Row>
                   <Row className="mt-5 master-grey color-secondary">
-                    {subscribedCount}  Subscribers
+                    {subscribedCount} Subscribers
                   </Row>
                 </div>
               )}
             </Col>
           </div>
         </Col>
-        <Col md="6" lg="6" sm="6">
+        <Col md="5" lg="4" sm="12">
           <img src={about_idea} width="100%"></img>
         </Col>
       </Row>
