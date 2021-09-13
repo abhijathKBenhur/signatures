6fb38fb5484401ab645045638a11443dcf79f0a0
abhijathbenhur <abhijath.benhur@tellius.com>
2021-09-13 11:59:13 +0530

    change referer'

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(
 
 app.use((req, res, next) => {
   
-  if (req.header("x-forwarded-proto") !== "https" && req.headers.referer.indexOf("localhost") == -1) {
+  if (req.header("x-forwarded-proto") !== "https") {
     res.redirect(`https://${req.header("host")}${req.url}`);
   } else {
     next();
