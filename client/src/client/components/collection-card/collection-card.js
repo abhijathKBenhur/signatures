d654f33b88bbf33144dab5836826c3caf4c8a9d0
sethulakshmy24 <sethu29286@gmail.com>
2021-09-27 13:27:10 +0530

    anim

--- client/src/client/components/collection-card/collection-card.js
+++ client/src/client/components/collection-card/collection-card.js
@@ -100,105 +100,85 @@ const CollectionCard = (props) => {
 
   return (
     <Col
-      key={signature._id}
-      className="main-container col-lg-2-4"
-      md="4"
-      lg="3"
-      sm="6"
-      xs="12"
-    >
-     <a href="#" class="card">
-	<div class="card__head">
-		<div class="card__image"></div>
-		<div class="card__author">
-			<div class="author">
-				<img src="https://s.gravatar.com/avatar/7ff9e93ff25e002bc49f4d69c0c3eac7?s=80" alt="Author of Tobias Reich" class="author__image"/>
-				<div class="author__content">
-					<p class="author__header">Tobias Reich</p>
-					<p class="author__subheader">Web developer and designer</p>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="card__body">
-		<h2 class="card__headline">Hover me</h2>
-		<p class="card__text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.</p>
-	</div>
-	<div class="card__foot">
-		<span class="card__link">Read more</span>
-	</div>
-	<div class="card__border"></div>
-</a>
-      {/* <div className="card-container">
-        <div className="card-float-header">
-          <div className="user-logo">
-            <Image src={_.get(signature,'owner.imageUrl')} color="F3F3F3" />
-            <div className="user-popup-outer">
-              <div className="user-popup">
-                <div className="user-logo">
-                  <Image src={_.get(signature,'owner.imageUrl')} />
-                </div>
-                <div>
-                  <Button disabled variant="pill">
-                    {JSON.parse(signature.category).label}
-                  </Button>
-                </div>
+    key={signature._id}
+    className="main-container col-lg-2-4"
+    md="4"
+    lg="3"
+    sm="6"
+    xs="12"
+  >
+    <div className="card-container">
+      <div className="card-float-header">
+        <div className="user-logo">
+          <Image src={_.get(signature,'owner.imageUrl')} color="F3F3F3" />
+          <div className="user-popup-outer">
+            <div className="user-popup">
+              <div className="user-logo">
+                <Image src={_.get(signature,'owner.imageUrl')} />
+              </div>
+              <div>
+                <Button disabled variant="pill">
+                  {JSON.parse(signature.category).label}
+                </Button>
               </div>
             </div>
           </div>
-          <div
-            className="user-name third-header ml-2"
-            onClick={() => {
-              goToUserProfile(_.get(signature,'owner.userName'));
-            }}
-          >
-            {_.get(signature,"owner.userName")}
-          </div>
         </div>
-        <div
-          className="image-container"
-          onClick={() => {
-            openCardView();
-          }}
+        <div className="user-name third-header ml-2"
+          // onClick={() => {
+          //   goToUserProfile(_.get(signature,'owner.userName'));
+          // }}
         >
-          <div className={_.get(JSON.parse(signature.category), "value") + " masking third-header h-100"}>
-            <div className="description">
-              {
-               getInitialSubString(signature.description, 40)}
-            </div>
-            <div className="title master-grey color-white h-100">
-            {getInitialSubString(signature.title,20)}
-            </div>
-          </div>
-          <Image
-            src={signature.thumbnail}
-            className="img-fluid"
-            style={{
-              background: "#f1f1f1",
-            }}
-          />
+          {_.get(signature, 'owner.userName') || signature.userName}
         </div>
-        <div className={getClassNames()}>
-          <div className="tag-n-location">
-            <div className=" second-header timestamp">
-              {moment(signature.createdAt).format("YYYY-MM-DD HH:mm:ss")}
+      </div>
+      <div
+        className="image-container"
+        onClick={() => {
+          openCardView();
+        }}
+      >
+          <a href="#" className={`card ${(_.get(JSON.parse(signature.category)[0], 'value'))}`}>
+            <div className="card__head" >
+              <div className="card__image" style={{ backgroundImage: `url(${signature.thumbnail}` }}></div>
+              <div className="card__author">
+                <div className="author">
+                  <div className="purpose-icons">
+                    <i
+                      className={getPurposeIcon(
+                        _.get(signature, "purpose.purposeType") ||
+                        _.get(signature, "purpose")
+                      )}
+                    ></i>
+                  </div>
+                  <div className="author__content">
+                    <p className="author__header">{signature.title}</p>
+                    <p className="author__subheader">{signature.description}</p>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="tags second-grey">
-              {signature.location || "Global"}
+            <div className="card__body">
+              <div className={getClassNames()}>
+                <div className="tag-n-location">
+                  <div className=" second-header timestamp">
+                    {moment(signature.createdAt).format("YYYY-MM-DD HH:mm:ss")}
+                  </div>
+                  <div className="tags second-grey">
+                    {signature.location || "Global"}
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="purpose-badge">
-            <i
-              className={getPurposeIcon(
-                _.get(signature, "purpose.purposeType") ||
-                  _.get(signature, "purpose")
-              )}
-            ></i>
-          </div>
-        </div>
-        {/* <div className="footer-area">testing bottom footer area</div> */}
-      {/* </div> */} 
-    </Col>
+            <div className="card__foot">
+            </div>
+            <div className="card__border"></div>
+          </a>
+      </div>
+      
+      {/* <div className="footer-area">testing bottom footer area</div> */}
+    </div>
+  </Col>
   );
 };
 export default CollectionCard;
