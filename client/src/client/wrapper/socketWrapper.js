747638410d72e39a08845101eb18d2688fe66adc
abhijathbenhur <abhijath.benhur@tellius.com>
2022-01-31 08:17:17 +0530

    Update socketWrapper.js

--- client/src/client/wrapper/socketWrapper.js
+++ client/src/client/wrapper/socketWrapper.js
@@ -4,7 +4,7 @@ let liveInstance = null;
 export const getNewConnection = metamaskID => {
     console.log("checking env")
     // let socketEndpoint = process.env.NODE_ENV == "production"  ? ENDPOINTS.WEBSOCKET_REMOTE_ENDPOINT : ENDPOINTS.WEBSOCKET_ENDPOINT
-    let socketEndpoint = "ws://testideatribe.herokuapp.com"
+    let socketEndpoint = "wss://testideatribe.herokuapp.com"
     console.log("socketEndpoint" , socketEndpoint)
     try{
         liveInstance = new WebSocket(socketEndpoint+"?metamaskId="+metamaskID);
